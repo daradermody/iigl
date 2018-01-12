ab3b98660611f311cf500e78b299f0ead0e8b602
Dara Dermody <dara.dermody@ericsson.com>
2018-01-12 16:24:36 +0000

    Revert "Adding Wall of Fame for teams that win tournaments."
    This reverts commit bac829eaef2d728992ee993782eae0f997dcbf38.

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,9 +1,15 @@
-import {Component} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
-export class MainComponent {
+export class MainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
 }
