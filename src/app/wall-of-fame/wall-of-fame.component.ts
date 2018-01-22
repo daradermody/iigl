1143acb3c59079cbcf990a75062e237e3a5dbadf
Dara Dermody <daradermody@gmail.com>
2018-01-22 10:48:19 +0000

    Adding Wall of Fame for teams that win tournaments (#2)
    Adding Wall of Fame for teams that win tournaments

---
+++ src/app/wall-of-fame/wall-of-fame.component.ts
@@ -0,0 +1,18 @@
+import {Component} from '@angular/core';
+import {WallOfFameGame} from '../wall-of-fame-game';
+
+@Component({
+  selector: 'app-wall-of-fame',
+  templateUrl: './wall-of-fame.component.html',
+  styleUrls: ['./wall-of-fame.component.css']
+})
+export class WallOfFameComponent {
+  games = [
+    new WallOfFameGame('Rocket League', 'assets/img/rocket_league_wall.png'),
+    new WallOfFameGame('League of Legends', 'assets/img/lol_wall.png'),
+    new WallOfFameGame('Overwatch', 'assets/img/overwatch_wall.png'),
+    new WallOfFameGame('Hearthstone', 'assets/img/hearthstone_wall.png')
+  ];
+
+  selectedGame: WallOfFameGame;
+}
