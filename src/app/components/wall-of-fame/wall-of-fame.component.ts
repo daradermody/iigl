5d24a86d6ad408d5b03f9b4db9c3afb5ba8ff7bb
Dara Dermody <daradermody@gmail.com>
2018-03-30 21:02:08 +0100

    Removed placeholder wall of fame images

--- src/app/components/wall-of-fame/wall-of-fame.component.ts
+++ src/app/components/wall-of-fame/wall-of-fame.component.ts
@@ -8,10 +8,7 @@ import {WallOfFameGame} from '../../data_types/wall-of-fame-game';
 })
 export class WallOfFameComponent {
   games = [
-    new WallOfFameGame('Rocket League', '/assets/img/wall_of_fame/rocket_league_wall.png'),
-    new WallOfFameGame('League of Legends', '/assets/img/wall_of_fame/lol_wall.png'),
-    new WallOfFameGame('Overwatch', '/assets/img/wall_of_fame/overwatch_wall.png'),
-    new WallOfFameGame('Hearthstone', '/assets/img/wall_of_fame/hearthstone_wall.png')
+    new WallOfFameGame('Rocket League', '/assets/img/wall_of_fame/placeholder.jpg'),
   ];
 
   selectedGame: WallOfFameGame;
