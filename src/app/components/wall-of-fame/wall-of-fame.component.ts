3644fe47d2684fee1802d3e7d86ff07e7fe634cf
Jason O'Connor <jaybopper@hotmail.com>
2020-03-02 23:07:45 +0000

    Fixed wall of fame component...

--- src/app/components/wall-of-fame/wall-of-fame.component.ts
+++ src/app/components/wall-of-fame/wall-of-fame.component.ts
@@ -9,7 +9,7 @@ import {WallOfFameGame} from '../../data_types/wall-of-fame-game';
 export class WallOfFameComponent {
   games = [
     new WallOfFameGame('Rocket League', '/assets/img/wall_of_fame/rocket_league_placeholder.jpg'),
-    new WallOfFameGame('League of Legends', '/assets/img/wall_of_fame/lol_placeholder.jpg')
+    new WallOfFameGame('League of Legends', '/assets/img/wall_of_fame/lol_placeholder.jpg'),
     new WallOfFameGame('Smash Ultimate', '/assets/img/wall_of_fame/smash_ultimate.png')
   ];
 
